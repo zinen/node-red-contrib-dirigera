397c3e990de58e5b260c56f8e3dd5bf59944cfb5
zinen <9595798+zinen@users.noreply.github.com>
2025-01-06 20:29:40 +0100

    Allow for input of room or device id now

--- dirigera.js
+++ dirigera.js
@@ -1,85 +1,79 @@
 module.exports = function (RED) {
   'use strict'
   const DirigeraHub = require('node-dirigera-promise')
+
+  const handleError = (res, errorText, done) => {
+    if (done) return
+    res.json(JSON.stringify({ error: errorText }))
+    done = true
+  }
+
   RED.httpAdmin.get('/ikeaDirigera/auth', RED.auth.needsPermission('dirigera.write'), async function (req, res) {
-    const onError = (errorText) => {
-      if (done) return
-      res.json(JSON.stringify({ error: errorText }))
-      done = true
-    }
     let done = false
     const hubAddress = String(req.query.hubAddress)
     if (!hubAddress) {
-      onError('Hub address empty. Missing ip or hostname to use.')
+      handleError(res, 'Hub address empty. Missing ip or hostname to use.', done)
       return
     }
     res.setTimeout(60000, function () {
-      onError('Button push was not registered at address: ' + hubAddress)
+      handleError(res, 'Button push was not registered at address: ' + hubAddress, done)
     })
     try {
-      const hubOptions = {}
-      hubOptions.hubAddress = hubAddress
-      hubOptions.clientName = req.query.clientName ? req.query.clientName : 'node-red-dirigera'
-      hubOptions.debug = 0
+      const hubOptions = {
+        hubAddress,
+        clientName: req.query.clientName || 'node-red-dirigera',
+        debug: 0
+      }
       const dirigeraHub = new DirigeraHub(hubOptions)
       res.json(JSON.stringify({ access_token: await dirigeraHub.getAccessToken() }))
       done = true
     } catch (error) {
-      onError('/ikeaDirigera/auth > getAccessToken:err> ' + String(error))
+      handleError(res, '/ikeaDirigera/auth > getAccessToken:err > ' + String(error), done)
     }
   })
+
   RED.httpAdmin.get('/ikeaDirigera/dirigera', RED.auth.needsPermission('dirigera.read'), async function (req, res) {
     const node = RED.nodes.getNode(req.query.nodeId) || {}
-    if (node.dirigeraClient) {
-      try {
-        const result = { specificDevice: [] }
-        for (const device of (await node.dirigeraClient.getDevice())) {
-          try {
-            const roomName = device.room && device.room.name ? device.room.name : '(no room defined)'
-            result.specificDevice.push({ name: `${device.type || ''} - ${roomName} - ${device.attributes.customName || ''}`, id: device.id })
-            if (device.type === 'gateway') continue
-            const roomID = device.room && device.room.id ? device.room.id : ''
-            if (!Object.prototype.hasOwnProperty.call(result, device.type)) {
-              result[device.type] = [{ name: roomName, id: roomID }]
-            } else if (Object.prototype.hasOwnProperty.call(result, device.type)) {
-              const hasObjectWithId = result[device.type].some((item) => item.id === roomID)
-              if (!hasObjectWithId) {
-                result[device.type].push({ name: roomName, id: roomID })
-              }
-            }
-          } catch (error) {
-            console.log('/ikeaDirigera/dirigera-API-Error--Start:')
-            console.log(error)
-            console.log('/ikeaDirigera/dirigera-API-Error--item:')
-            console.log(device)
-            console.log('/ikeaDirigera/dirigera-API-Error--end.')
-          }
-        }
-        // Sort specificDevice list by its name
-        result.specificDevice = result.specificDevice.sort((a, b) => { return a.name < b.name ? -1 : 1 })
-        for (const scene of (await node.dirigeraClient.getScene())) {
-          if (!Object.prototype.hasOwnProperty.call(result, 'scene')) {
-            result.scene = [{ name: scene.info.name, id: scene.id }]
-          } else {
-            result.scene.push({ name: scene.info.name, id: scene.id })
-          }
-        }
-        res.json(JSON.stringify(result))
-      } catch (error) {
-        res.json('{"Error see console":[{"name":"' + String(error) + '", "id":-1}]}')
-        node.error('Dirigera get devices error: ' + error)
-      }
-    } else {
+    if (!node.dirigeraClient) {
       res.json('{"Make and deploy config first":[{"name":"Make and deploy config first", "id":-1}]}')
+      return
+    }
+    try {
+      const result = { specificDevice: [] }
+      const devices = await node.dirigeraClient.getDevice()
+      for (const device of devices) {
+        try {
+          const roomName = device.room?.name || '(no room defined)'
+          result.specificDevice.push({ name: `${device.type || ''} - ${roomName} - ${device.attributes.customName || ''}`, id: device.id })
+          if (device.type === 'gateway') continue
+          const roomID = device.room?.id || ''
+          if (!result[device.type]) {
+            result[device.type] = [{ name: 'Override (msg.roomId)', id: 'overrideRoom' }, { name: roomName, id: roomID }]
+          } else if (!result[device.type].some(item => item.id === roomID)) {
+            result[device.type].push({ name: roomName, id: roomID })
+          }
+        } catch (error) {
+          console.error('/ikeaDirigera/dirigera-API-Error:', error, device)
+        }
+      }
+      result.specificDevice.sort((a, b) => a.name.localeCompare(b.name))
+      result.specificDevice.unshift({ name: 'Override (msg.deviceId)', id: 'overrideDevice' })
+      const scenes = await node.dirigeraClient.getScene()
+      result.scene = scenes.map(scene => ({ name: scene.info.name, id: scene.id }))
+      res.json(JSON.stringify(result))
+    } catch (error) {
+      res.json('{"Error see console":[{"name":"' + String(error) + '", "id":-1}]}')
+      node.error('Dirigera get devices error: ' + error)
     }
   })
+
   function DirigeraConfigNode (n) {
     RED.nodes.createNode(this, n)
     const node = this
     if (!node.credentials.hubAddress || !node.credentials.hubAccessCode) return
-    const temp = new DirigeraHub({ hubAddress: node.credentials.hubAddress, access_token: node.credentials.hubAccessCode, debug: 0 })
-    temp.logIn().then(() => {
-      node.dirigeraClient = temp
+    const dirigeraHub = new DirigeraHub({ hubAddress: node.credentials.hubAddress, access_token: node.credentials.hubAccessCode, debug: 0 })
+    dirigeraHub.logIn().then(() => {
+      node.dirigeraClient = dirigeraHub
       node.lastError = ''
     }).catch((error) => {
       node.dirigeraClient = undefined
@@ -112,19 +106,29 @@ module.exports = function (RED) {
         if (node.config.choiceType === 'scene') {
           await node.server.dirigeraClient.triggerScene(node.config.choiceId)
         } else if (node.config.choiceType === 'specificDevice') {
+          let deviceId = node.config.choiceId
+          if (node.config.choiceId === 'overrideDevice') {
+            if (!msg.deviceId) throw new Error('No deviceId in msg.deviceId')
+            deviceId = msg.deviceId
+          }
           if (msg.topic) {
-            msg.payload = await node.server.dirigeraClient.setDevice(node.config.choiceId, msg.topic, msg.payload)
+            msg.payload = await node.server.dirigeraClient.setDevice(deviceId, msg.topic, msg.payload)
           } else {
-            msg.payload = await node.server.dirigeraClient.getDevice(node.config.choiceId)
+            msg.payload = await node.server.dirigeraClient.getDevice(deviceId)
             msg.availableTopics = msg.payload.capabilities.canReceive
           }
           send(msg)
         } else {
+          let roomId = node.config.choiceId
+          if (node.config.choiceId === 'overrideRoom') {
+            if (!msg.roomId) throw new Error('No roomId in msg.roomId')
+            roomId = msg.roomId
+          }
           if (msg.topic) {
-            msg.payload = await node.server.dirigeraClient.setRoom(node.config.choiceId, msg.topic, msg.payload, node.config.choiceType)
+            msg.payload = await node.server.dirigeraClient.setRoom(roomId, msg.topic, msg.payload, node.config.choiceType)
             if (msg.payload.ok.length === 0 && msg.payload.errors.length > 0) throw new Error(msg.payload.errors[0])
           } else {
-            msg.payload = await node.server.dirigeraClient.getRoom(node.config.choiceId, node.config.choiceType)
+            msg.payload = await node.server.dirigeraClient.getRoom(roomId, node.config.choiceType)
             msg.availableTopics = msg.payload[0].capabilities.canReceive
           }
           send(msg)
